290f7f2ca714730e3364f87df20cdf176e0e1f5d
Raul Silva <raulsilva@Rauls-MacBook-Pro.local>
2018-08-16 17:45:30 -0300

    starts project

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://admin:admin123@ds121332.mlab.com:21332/devconnector"
+};
---
+++ routes/api/user.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+// Load Model
+const User = require('../../models/User');
+
+/**
+ * @route GET api/users/test
+ * @desc  Tests users route
+ * @access Public
+ */
+router.get('/test', (req, res) => res.json({ status: 'users' }));
+
+/**
+ * @route POST api/users/register
+ * @desc  Register user
+ * @access Public
+ */
+router.post('/register', (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: 'Email already existis' });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: '200', // Size
+        r: 'pg', // Rating
+        d: 'mm', // default
+      });
+
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password,
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => {
+              res.json(user);
+            })
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
+
+module.exports = router;
